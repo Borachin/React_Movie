a86ecd64c8a824bf6973fea2c62046a309426ad9
Borachin <chinbora92@naver.com>
2021-01-23 15:50:00 +0900

    commit react practise

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="container">
+      <div className="container">
+          <div className="login-form">
+              <h4>로그인 박스</h4>
+              <label>ID</label>
+              <input type="text" id="loginid" placeholder="ID를 입력하세요"></input>
+
+              <label>Password</label>
+              <input type="text" id="loginpw" placeholder="PW를 입력하세요"></input>
+              
+              <button type="submit" onclick="loginsubmit()">Login</button>
+          </div>
+      </div>
     </div>
   );
 }
